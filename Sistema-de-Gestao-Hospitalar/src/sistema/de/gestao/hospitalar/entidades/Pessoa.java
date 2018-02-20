6b328e6ec26e08c425a38b4964ad6f1e429da29e
Francisco Leonésio Carneiro Duarte <leonesiorf@hotmail.com>
2018-02-20 10:33:54 -0300

    Criação de projeto no netbean e criação da classe pessoa

---
+++ Sistema-de-Gestao-Hospitalar/src/sistema/de/gestao/hospitalar/entidades/Pessoa.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistema.de.gestao.hospitalar.entidades;
+
+import java.util.Date;
+
+/**
+ *
+ * @author leone
+ */
+public class Pessoa {
+    protected int id;
+    protected String nome;
+    protected String cpf;
+    protected String rg;
+    protected Date data_nascimento;
+    protected String telefone;
+    protected String cidade;
+    protected String bairro;
+    protected String logradouro;
+    protected String cep;
+}
