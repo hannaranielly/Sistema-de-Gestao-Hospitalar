61f47071d4bfd0f6e75b99adbbcbb17bb918218e
Jefferson John <jefferson.john.rego@gmail.com>
2018-02-21 17:56:45 -0300

    Organização da classe administrador

--- Sistema-de-Gestao-Consultas/src/sistema/de/gestao/consultas/entidades/Administrador.java
+++ Sistema-de-Gestao-Consultas/src/sistema/de/gestao/consultas/entidades/Administrador.java
@@ -7,12 +7,7 @@ package sistema.de.gestao.consultas.entidades;
 
 import java.io.Serializable;
 import java.util.Date;
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 
 /**
