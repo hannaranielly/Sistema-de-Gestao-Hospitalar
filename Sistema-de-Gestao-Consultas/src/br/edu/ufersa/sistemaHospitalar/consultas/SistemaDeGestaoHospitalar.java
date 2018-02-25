8896269692caab43056a7f9eab59034faf6afdf9
Leonésio <leonesiorf@hotmail.com>
2018-02-25 12:06:04 -0300

    retirado códigos de teste em comentário da classe principal

--- Sistema-de-Gestao-Consultas/src/br/edu/ufersa/sistemaHospitalar/consultas/SistemaDeGestaoHospitalar.java
+++ Sistema-de-Gestao-Consultas/src/br/edu/ufersa/sistemaHospitalar/consultas/SistemaDeGestaoHospitalar.java
@@ -27,56 +27,13 @@ public class SistemaDeGestaoHospitalar {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        /* Exemplo uso Hibernate
-        SessionFactory sf = HibernateUtil.getSessionFactory();
-        Session session = sf.openSession();
-        Paciente p = new Paciente();
-        p.setNome("Francisco Leonésio Carneiro Duarte");
-        p.setBairro("Centro");
-        p.setBairro("Centro");
-        p.setNum_sus("123456");
-        //p.setId(1l);
-        Transaction tx = session.beginTransaction();
-        session.saveOrUpdate(p);
-        tx.commit();
-        Administrador adb = (Administrador)session.get(Administrador.class, 1);
-        System.out.println(adb.user);
-        System.out.println(adb.senha);
-        session.flush();
-        session.close();
-        sf.close();*/
-        
-        /*
-        SessionFactory sf = HibernateUtil.getSessionFactory();
-        Session session = sf.openSession();
-        Administrador p = new Administrador("eu", "eu");
-        //p.setId(1l);
-        Transaction tx = session.beginTransaction();
-        session.saveOrUpdate(p);
-        
-        tx.commit();
-        
-        session.flush();
-        session.close();
-        sf.close();*/
-        System.out.println(Criptografia.criptografar("admin"));
+       
         TelaLogin tela = new TelaLogin();
         tela.setVisible(true);
         tela.setLocationRelativeTo(null);
         
         
-        
-       // Administrador adm = new Administrador("admin", "admin");
-       // adm.validaLogin();
-        /*
-        SessionFactory sf = HibernateUtil.getSessionFactory();
-        Session session = sf.openSession();
-        Query query = session.createSQLQuery("select * from administrador where id = 1").addEntity(Administrador.class);
-        List<Administrador> list =  (List<Administrador>)query.list();
-        System.out.println(list.get(0).getUser());
-        session.flush();
-        session.close();*/
+     
     }
     
 }
