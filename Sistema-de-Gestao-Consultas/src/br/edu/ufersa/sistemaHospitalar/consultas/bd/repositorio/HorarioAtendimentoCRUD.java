e7cb9151342b1939921d4e4e1c37381deaca3b84
Leonésio <leonesiorf@hotmail.com>
2018-02-26 19:30:33 -0300

    inicio implementação validação addhorario

--- Sistema-de-Gestao-Consultas/src/br/edu/ufersa/sistemaHospitalar/consultas/bd/repositorio/HorarioAtendimentoCRUD.java
+++ Sistema-de-Gestao-Consultas/src/br/edu/ufersa/sistemaHospitalar/consultas/bd/repositorio/HorarioAtendimentoCRUD.java
@@ -7,7 +7,11 @@ package br.edu.ufersa.sistemaHospitalar.consultas.bd.repositorio;
 
 import br.edu.ufersa.sistemaHospitalar.consultas.bd.HibernateUtil;
 import br.edu.ufersa.sistemaHospitalar.consultas.entidades.HorarioAtendimento;
+import br.edu.ufersa.sistemaHospitalar.consultas.entidades.Medico;
+import java.sql.Time;
+import java.util.List;
 import org.hibernate.HibernateException;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -46,5 +50,21 @@ public class HorarioAtendimentoCRUD {
         } catch (HibernateException ex) {
             ex.printStackTrace();
         }
+    }
+    
+    public List<HorarioAtendimento> consulta_por_id_medico(int id, Time inicio, Time fim){
+        Session session = sf.openSession();
+        Query query = session.createSQLQuery("select * from horario_atendimento AS h where h.medico_id = ? and (max(?,h.inicio) <= min(?,h.fim))").addEntity(HorarioAtendimento.class);
+        query.setInteger(0, id);
+        query.setDate(1, inicio);
+        query.setDate(2, fim);
+        List<HorarioAtendimento> list =  (List<HorarioAtendimento>) query.list();
+        session.close();
+        if(list.isEmpty()){
+            return null;
+        }else{
+            return list;
+        }
+        
     }
 }
