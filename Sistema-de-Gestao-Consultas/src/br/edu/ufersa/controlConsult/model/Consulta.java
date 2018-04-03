5dcb7d101f7d65090c1bce96ce6331fe8c8de515
Juan Carlos <juanksoliveira@gmail.com>
2018-04-03 10:37:11 -0300

    Adicionado Entidade e Controlador de Consulta.

---
+++ Sistema-de-Gestao-Consultas/src/br/edu/ufersa/controlConsult/model/Consulta.java
@@ -0,0 +1,127 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ufersa.controlConsult.model;
+
+import br.edu.ufersa.controlConsult.model.interfaces.ICRUD;
+import br.edu.ufersa.controlConsult.model.jpaDAO.exceptions.NonexistentEntityException;
+import br.edu.ufersa.controlConsult.model.jpaDAO.exceptions.PreexistingEntityException;
+import java.io.Serializable;
+import java.sql.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author juan
+ */
+@Entity
+@Table(name = "consulta")
+public class Consulta implements Serializable, ICRUD {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+
+    @Column(name = "agendada")
+    boolean agendada;
+
+    @Column(name = "data_inicio")
+    @Temporal(TemporalType.TIMESTAMP)
+    Date data_inicio;
+
+    @Column(name = "data_fim")
+    @Temporal(TemporalType.TIMESTAMP)
+    Date data_fim;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "medico")
+    Medico medico;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "paciente")
+    Paciente paciente;
+
+    public Consulta() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public boolean isAgendada() {
+        return agendada;
+    }
+
+    public void setAgendada(boolean agendada) {
+        this.agendada = agendada;
+    }
+
+    public Date getData_inicio() {
+        return data_inicio;
+    }
+
+    public void setData_inicio(Date data_inicio) {
+        this.data_inicio = data_inicio;
+    }
+
+    public Date getData_fim() {
+        return data_fim;
+    }
+
+    public void setData_fim(Date data_fim) {
+        this.data_fim = data_fim;
+    }
+
+    public Medico getMedico() {
+        return medico;
+    }
+
+    public void setMedico(Medico medico) {
+        this.medico = medico;
+    }
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }
+
+    @Override
+    public void create() throws PreexistingEntityException, Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void read() throws NonexistentEntityException, Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void update() throws NonexistentEntityException, Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete() throws NonexistentEntityException, Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
