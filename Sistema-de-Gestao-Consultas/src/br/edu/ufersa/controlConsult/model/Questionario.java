86ffd29218fbd2f3afbebe805dd55cf5eb337c1e
Francisco Leonésio Carneiro Duarte <leonesiorf@hotmail.com>
2018-04-07 16:09:14 -0300

    instegração média das questões no desempenho do médico

--- Sistema-de-Gestao-Consultas/src/br/edu/ufersa/controlConsult/model/Questionario.java
+++ Sistema-de-Gestao-Consultas/src/br/edu/ufersa/controlConsult/model/Questionario.java
@@ -20,6 +20,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 /**
@@ -28,6 +30,7 @@ import javax.persistence.Table;
  */
 @Entity
 @Table(name = "questionario")
+
 public class Questionario implements Serializable, ICRUD{
     @Id
     @Column(name = "id")
