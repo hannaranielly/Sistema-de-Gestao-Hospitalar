6631a03c2960df449809224ee444a08b8a9284f5
Juan Carlos <juanksoliveira@gmail.com>
2018-04-08 16:27:49 -0300

    Add: MenuItem em TelaInicial com: "Listar Pacientes".

--- Sistema-de-Gestao-Consultas/src/br/edu/ufersa/controlConsult/gui/TelaInicial.java
+++ Sistema-de-Gestao-Consultas/src/br/edu/ufersa/controlConsult/gui/TelaInicial.java
@@ -39,6 +39,7 @@ public class TelaInicial extends javax.swing.JFrame {
         jMenuItem2 = new javax.swing.JMenuItem();
         jMenuItem3 = new javax.swing.JMenuItem();
         jMenuItem6 = new javax.swing.JMenuItem();
+        jMenuItem5 = new javax.swing.JMenuItem();
         jMenu2 = new javax.swing.JMenu();
         jMenuItem4 = new javax.swing.JMenuItem();
         jMenuItem1 = new javax.swing.JMenuItem();
@@ -120,6 +121,14 @@ public class TelaInicial extends javax.swing.JFrame {
             }
         });
         jMenu1.add(jMenuItem6);
+
+        jMenuItem5.setText("Listar Pacientes");
+        jMenuItem5.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem5ActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jMenuItem5);
 
         jMenuBar1.add(jMenu1);
 
@@ -321,6 +330,12 @@ public class TelaInicial extends javax.swing.JFrame {
         sm.setLocationRelativeTo(null);
     }//GEN-LAST:event_jMenuItem15ActionPerformed
 
+    private void jMenuItem5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem5ActionPerformed
+        // Listar Pacientes
+        ListarPessoa lp = new ListarPessoa(TipoPessoaEnum.PACIENTE);
+        lp.setVisible(true);
+    }//GEN-LAST:event_jMenuItem5ActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -378,6 +393,7 @@ public class TelaInicial extends javax.swing.JFrame {
     private javax.swing.JMenuItem jMenuItem2;
     private javax.swing.JMenuItem jMenuItem3;
     private javax.swing.JMenuItem jMenuItem4;
+    private javax.swing.JMenuItem jMenuItem5;
     private javax.swing.JMenuItem jMenuItem6;
     private javax.swing.JMenuItem jMenuItem8;
     private javax.swing.JMenuItem jMenuItem9;
