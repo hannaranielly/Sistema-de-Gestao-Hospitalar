4d3442ff67677ecfec540dd61f0dc12dac258621
Juan Carlos <juanksoliveira@gmail.com>
2018-04-09 22:59:23 -0300

    Update: correção do redimensionamento vertical.

--- Sistema-de-Gestao-Consultas/src/br/edu/ufersa/controlConsult/gui/TelaInicial.java
+++ Sistema-de-Gestao-Consultas/src/br/edu/ufersa/controlConsult/gui/TelaInicial.java
@@ -98,8 +98,8 @@ public class TelaInicial extends javax.swing.JFrame {
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addComponent(jDesktopPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jDesktopPane1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 85, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
         );
